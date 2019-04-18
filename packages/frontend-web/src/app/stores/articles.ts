6b1e5b9d956e631c5538b6bb660908c3689666b6
Alessandro Alempijevic <alessandro.alempijevic@nytimes.com>
2019-04-18 12:50:55 -0400

    Temporarily disable article requests over socket

--- packages/frontend-web/src/app/stores/articles.ts
+++ packages/frontend-web/src/app/stores/articles.ts
@@ -35,7 +35,7 @@ export function getArticles(state: IAppStateRecord): Map<ModelId, IArticleModel>
 export function getArticle(state: IAppStateRecord, articleId: ModelId): IArticleModel {
   const article = getArticles(state).get(articleId);
   if (!article) {
-    requestArticle(articleId);
+    // requestArticle(articleId);
     // Temporarily respond with an empty article while we fetch the actual data over the socket
     return ArticleModel({
       id: articleId,
