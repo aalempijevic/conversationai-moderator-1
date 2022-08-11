5304b4b256b5c6c1c45dbf3fae3bdf9ea6635d0e
Jake O'Toole <jake.otoole@nytimes.com>
2022-08-11 13:43:18 -0500

    Created fake data to dev layout for banned keyword settings ui. Started building out table for banned keywords.

--- packages/frontend-web/src/app/scenes/Settings/sections/BannedKeywords.tsx
+++ packages/frontend-web/src/app/scenes/Settings/sections/BannedKeywords.tsx
@@ -1,18 +1,55 @@
 import { css } from "../../../utilx";
+import { SETTINGS_STYLES } from "../settingsStyles";
 
 export interface ISection {
   styles?: any;
 }
 
+const TEMP_KEYWORDS = [
+  "dog",
+  "basketball",
+  "soccer",
+  "carrot",
+  "lamp",
+  "tree",
+  "continent",
+];
+
 export default function ({ styles }: ISection) {
   return (
     <div key="bannedKeywordsSection">
       <div key="heading" {...css(styles.heading)}>
-        <h2 {...css(styles.headingText)}>BannedKeywords</h2>
+        <h2 {...css(styles.headingText)}>Banned Keywords</h2>
       </div>
       <div key="body" {...css(styles.section)}>
         {/* Temporary wording. May need updating */}
         <p>These keywords are banned and will cause a comment to be flagged.</p>
+        <div {...css(SETTINGS_STYLES.row)}>
+          <table>
+            <thead>
+              <tr>
+                <th key="keywordWordColumn" {...css(SETTINGS_STYLES.userTableCell)}>
+                  Keyword
+                </th>
+                <th key="keywordActionColumn" {...css(SETTINGS_STYLES.userTableCell)}>
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              
+        {TEMP_KEYWORDS.map((keyword)=> (
+        <tr key={`banned-keyword-${keyword}`} {...css(SETTINGS_STYLES.userTableCell)}>
+          <td {...css(SETTINGS_STYLES.userTableCell)}>
+            {keyword}
+          </td>
+          <td {...css(SETTINGS_STYLES.userTableCell)}>
+            Put severity selector here.
+          </td>
+        </tr>))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
