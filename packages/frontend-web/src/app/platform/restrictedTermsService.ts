575d517f8bf867c1db577049840c2f0063c8274f
Jake O'Toole <jake.otoole@nytimes.com>
2022-09-01 15:04:29 -0500

    Defining a function for each endpoint related to restrictedTerms.

--- packages/frontend-web/src/app/platform/restrictedTermsService.ts
+++ packages/frontend-web/src/app/platform/restrictedTermsService.ts
@@ -4,6 +4,7 @@ import { List } from "immutable";
 import { serviceURL } from "./dataService";
 import { IRestrictedTermModel, RestrictedTermModel } from "../../models";
 
+// /services/simple/restrictedTerms
 export async function getRestrictedTerms(): Promise<List<IRestrictedTermModel>> {
   const getRestrictedTermsUrl = serviceURL("simple", "/restrictedTerms");
   console.log("get URL", getRestrictedTermsUrl);
@@ -15,3 +16,12 @@ export async function getRestrictedTerms(): Promise<List<IRestrictedTermModel>> 
     })
   );
 }
+
+// /services/simple/restrictedTerms
+export async function addRestrictedTerm() {}
+
+// /services/simple/restrictedTerms
+export async function updateRestrictedTerm() {}
+
+// /services/simple/restrictedTerms/{termId}
+export async function deleteRestrictedTerm() {}
