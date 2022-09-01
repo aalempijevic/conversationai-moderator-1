e780b91c75c288cbfc91d839720a97d47636c4e3
Jake O'Toole <jake.otoole@nytimes.com>
2022-09-01 12:25:17 -0500

    Switched RestrictedTerms from a function to a class. Current version of React does not support all the necessary features for using functional components.

--- packages/frontend-web/src/app/scenes/Settings/sections/RestrictedTerms.tsx
+++ packages/frontend-web/src/app/scenes/Settings/sections/RestrictedTerms.tsx
@@ -1,7 +1,9 @@
+import { Component } from "react";
+
 import { css } from "../../../utilx";
 import { SETTINGS_STYLES } from "../settingsStyles";
 
-export interface ISection {
+export interface ISectionProps {
   styles?: any;
   settingsState: any;
   setSettingsState: any;
@@ -17,83 +19,91 @@ const TEMP_KEYWORDS = [
   "continent",
 ];
 
-export default function ({ styles }: ISection) {
-  return (
-    <div key="bannedKeywordsSection">
-      <div key="heading" {...css(styles.heading)}>
-        <h2 {...css(styles.headingText)}>Banned Keywords</h2>
-      </div>
-      <div key="body" {...css(styles.section)}>
-        {/* Temporary wording. May need updating */}
-        <p>These keywords are banned and will cause a comment to be flagged.</p>
-        <div {...css(SETTINGS_STYLES.row)}>
-          <table>
-            <thead>
-              <tr>
-                <th
-                  key="keywordWordColumn"
-                  {...css(SETTINGS_STYLES.userTableCell)}
-                >
-                  Keyword
-                </th>
-                <th
-                  key="keywordActionColumn"
-                  {...css(SETTINGS_STYLES.userTableCell)}
-                >
-                  Level
-                </th>
-                <th
-                  key="keywordDeleteColumn"
-                  {...css(SETTINGS_STYLES.userTableCell)}
-                >
-                  Delete
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {TEMP_KEYWORDS.map((keyword) => (
-                <tr
-                  key={`banned-keyword-${keyword}`}
-                  {...css(SETTINGS_STYLES.userTableCell)}
-                >
-                  <td {...css(SETTINGS_STYLES.userTableCell)}>{keyword}</td>
-                  <td {...css(SETTINGS_STYLES.userTableCell)}>
-                    <input
-                      type="radio"
-                      id={`${keyword}-reject`}
-                      name={`keyword-${keyword}-level`}
-                      value="reject"
-                    />
-                    <label htmlFor={`${keyword}-reject`}>Reject</label>
-                    <input
-                      type="radio"
-                      id={`${keyword}-defer`}
-                      name={`keyword-${keyword}-level`}
-                      value="defer"
-                    />
-                    <label htmlFor={`${keyword}-reject`}>Defer</label>
-                    <input
-                      type="radio"
-                      id={`${keyword}-warn`}
-                      name={`keyword-${keyword}-level`}
-                      value="warn"
-                    />
-                    <label htmlFor={`${keyword}-reject`}>Warn</label>
-                  </td>
-                  <td {...css(SETTINGS_STYLES.userTableCell)}>
-                    <input type="button" value="delete" />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+export class RestrictedTerms extends Component<ISectionProps> {
+  render() {
+    const { styles } = this.props;
+
+    return (
+      <div key="bannedKeywordsSection">
+        <div key="heading" {...css(styles.heading)}>
+          <h2 {...css(styles.headingText)}>Banned Keywords</h2>
         </div>
+        <div key="body" {...css(styles.section)}>
+          {/* Temporary wording. May need updating */}
+          <p>
+            These keywords are banned and will cause a comment to be flagged.
+          </p>
+          <div {...css(SETTINGS_STYLES.row)}>
+            <table>
+              <thead>
+                <tr>
+                  <th
+                    key="keywordWordColumn"
+                    {...css(SETTINGS_STYLES.userTableCell)}
+                  >
+                    Keyword
+                  </th>
+                  <th
+                    key="keywordActionColumn"
+                    {...css(SETTINGS_STYLES.userTableCell)}
+                  >
+                    Level
+                  </th>
+                  <th
+                    key="keywordDeleteColumn"
+                    {...css(SETTINGS_STYLES.userTableCell)}
+                  >
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {TEMP_KEYWORDS.map((keyword) => (
+                  <tr
+                    key={`banned-keyword-${keyword}`}
+                    {...css(SETTINGS_STYLES.userTableCell)}
+                  >
+                    <td {...css(SETTINGS_STYLES.userTableCell)}>{keyword}</td>
+                    <td {...css(SETTINGS_STYLES.userTableCell)}>
+                      <input
+                        type="radio"
+                        id={`${keyword}-reject`}
+                        name={`keyword-${keyword}-level`}
+                        value="reject"
+                      />
+                      <label htmlFor={`${keyword}-reject`}>Reject</label>
+                      <input
+                        type="radio"
+                        id={`${keyword}-defer`}
+                        name={`keyword-${keyword}-level`}
+                        value="defer"
+                      />
+                      <label htmlFor={`${keyword}-reject`}>Defer</label>
+                      <input
+                        type="radio"
+                        id={`${keyword}-warn`}
+                        name={`keyword-${keyword}-level`}
+                        value="warn"
+                      />
+                      <label htmlFor={`${keyword}-reject`}>Warn</label>
+                    </td>
+                    <td {...css(SETTINGS_STYLES.userTableCell)}>
+                      <input type="button" value="delete" />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
           <div>
             <label htmlFor="new-keyword">New Keyword</label>
             <input type="text" id="new-keyword" />
             <input type="button" value="add" />
           </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default RestrictedTerms;
