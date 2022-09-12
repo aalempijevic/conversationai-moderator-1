20b0da654aa1d98ff4c3fa120d85a392f58e2b1f
Jake O'Toole <jake.otoole@nytimes.com>
2022-09-12 15:24:52 -0500

    Switched more uses of the phrasing banned keywords to restricted terms.

--- packages/frontend-web/src/app/scenes/Settings/sections/RestrictedTerms.tsx
+++ packages/frontend-web/src/app/scenes/Settings/sections/RestrictedTerms.tsx
@@ -16,7 +16,7 @@ export interface ISectionProps {
 
 export interface IRestrictedTermsState {}
 
-const TEMP_KEYWORDS = ["dog", "basketball", "soccer", "carrot", "lamp", "tree", "continent"];
+const TEMP_TERMS = ["dog", "basketball", "soccer", "carrot", "lamp", "tree", "continent"];
 
 async function getTerms() {
   // get rid of Try/Catch when done with dev
@@ -41,37 +41,37 @@ export class RestrictedTerms extends Component<ISectionProps, IRestrictedTermsSt
     return (
       <div key="restrictedTermsSection">
         <div key="heading" {...css(styles.heading)}>
-          <h2 {...css(styles.headingText)}>Banned Keywords</h2>
+          <h2 {...css(styles.headingText)}>Restricted Terms</h2>
         </div>
         <div key="body" {...css(styles.section)}>
           {/* Temporary wording. May need updating */}
-          <p>These keywords are banned and will cause a comment to be flagged.</p>
+          <p>These terms are restricted and will cause a comment to be flagged.</p>
           <div {...css(SETTINGS_STYLES.row)}>
             <table>
               <thead>
                 <tr>
-                  <th key="keywordWordColumn" {...css(SETTINGS_STYLES.userTableCell)}>
-                    Keyword
+                  <th key="termWordColumn" {...css(SETTINGS_STYLES.userTableCell)}>
+                    Term
                   </th>
-                  <th key="keywordActionColumn" {...css(SETTINGS_STYLES.userTableCell)}>
+                  <th key="termLevelColumn" {...css(SETTINGS_STYLES.userTableCell)}>
                     Level
                   </th>
-                  <th key="keywordDeleteColumn" {...css(SETTINGS_STYLES.userTableCell)}>
+                  <th key="termDeleteColumn" {...css(SETTINGS_STYLES.userTableCell)}>
                     Delete
                   </th>
                 </tr>
               </thead>
               <tbody>
-                {TEMP_KEYWORDS.map((keyword) => (
-                  <tr key={`banned-keyword-${keyword}`} {...css(SETTINGS_STYLES.userTableCell)}>
-                    <td {...css(SETTINGS_STYLES.userTableCell)}>{keyword}</td>
+                {TEMP_TERMS.map((term) => (
+                  <tr key={`banned-term-${term}`} {...css(SETTINGS_STYLES.userTableCell)}>
+                    <td {...css(SETTINGS_STYLES.userTableCell)}>{term}</td>
                     <td {...css(SETTINGS_STYLES.userTableCell)}>
-                      <input type="radio" id={`${keyword}-reject`} name={`keyword-${keyword}-level`} value="reject" />
-                      <label htmlFor={`${keyword}-reject`}>Reject</label>
-                      <input type="radio" id={`${keyword}-defer`} name={`keyword-${keyword}-level`} value="defer" />
-                      <label htmlFor={`${keyword}-reject`}>Defer</label>
-                      <input type="radio" id={`${keyword}-warn`} name={`keyword-${keyword}-level`} value="warn" />
-                      <label htmlFor={`${keyword}-reject`}>Warn</label>
+                      <input type="radio" id={`${term}-reject`} name={`term-${term}-level`} value="reject" />
+                      <label htmlFor={`${term}-reject`}>Reject</label>
+                      <input type="radio" id={`${term}-defer`} name={`term-${term}-level`} value="defer" />
+                      <label htmlFor={`${term}-reject`}>Defer</label>
+                      <input type="radio" id={`${term}-warn`} name={`term-${term}-level`} value="warn" />
+                      <label htmlFor={`${term}-reject`}>Warn</label>
                     </td>
                     <td {...css(SETTINGS_STYLES.userTableCell)}>
                       <input type="button" value="delete" />
