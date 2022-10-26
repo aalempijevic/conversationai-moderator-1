f8d779f2d807746bb8b163a9823657f33ed190dc
Alessandro Alempijevic <aalempijevic@gmail.com>
2022-10-26 15:56:08 -0400

    fix path

--- packages/frontend-web/tooling/server.js
+++ packages/frontend-web/tooling/server.js
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const { makeServer } = require('./core');
+const { makeServer } = require('./core/server');
 const { mountWebFrontend } = require('../dist-commonjs/index');
 
 // Start up the app
